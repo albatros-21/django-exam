ab33be1dea85ee01baae1a193d5cc80b3596e6bd
albatros-21 <05170000782@ogrenci.ege.edu.tr>
2021-02-24 14:39:32 +0300

    first commit

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\bahar\Desktop\Online-Exam-System-master\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
